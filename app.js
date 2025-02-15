95d18e1ed6851c21307ac31af4544f48c642ca0d
Lucas Quintanilha <l.quitan@gmail.com>
2025-02-15 00:29:17 -0300

    Add information on .env file

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ import router from './src/routes/routes.js';
 import connectDatabase from './src/database/index.js';
 
 const app = express();
-
+const port = process.env.PORT || 4000;
 connectDatabase();
 
 app.use(router);
 
-app.listen(3000, () => {
-  console.log('🚀 Express Server is Running');
+app.listen(port, () => {
+  console.log(`Express Server is Running on ${port}`);
 });
